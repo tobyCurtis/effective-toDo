577849760784fdec4896b5417210431647806336
Toby <curti024@csusm.edu>
2016-05-03 16:40:36 -0700

    updated style to my liking, added input validation

--- index.js
+++ index.js
@@ -3,22 +3,24 @@ var app = angular.module("myApp", []);
 
 
 //starting my controller to house all my business
-app.controller("myCtrl", function($scope)
-{
+app.controller("myCtrl", function($scope) {
     //initialize, initialize, initialize, initialize
     $scope.todos = [];
     $scope.orderMethod = '';
+    $scope.task='';
 
     //function to add items to array that ng-repeat is watching
-    $scope.addTask = function()
-    {
-        //add 
-        $scope.todos.push(
-        {
-            'task': $scope.task,
-            'priority': $scope.color,
-        });
-        //reset the field
-        $scope.task = '';
+    $scope.addTask = function() {
+        if ($scope.task == '') {
+            alert("Please Type a Task");
+        } else {
+            //add 
+            $scope.todos.push({
+                'task': $scope.task,
+                'priority': $scope.color,
+            });
+            //reset the field
+            $scope.task = '';
+        }
     };
 });
